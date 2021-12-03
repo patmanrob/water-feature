7178fbfd289a887bf78bedfa6cf1fb0b594c961b
Rob <git@thepatman.co.uk>
2021-12-03 14:52:23 +0000

    Create fountainvariables.py

---
+++ fountainvariables.py
@@ -0,0 +1,7 @@
+email_user = "[User name for email to be sent]"
+email_password = "Password for email to be sent"
+email_send = "[email to send to]"
+url = '[URL to scrape sunrise/sunset data from]'
+send= True #True to send notification emails
+
+body=""#empty string for email body to be stored in
